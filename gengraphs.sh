1ac22db25bdd5cddb3e2a1f82ca26b8f56e72e6c
jlawson <jlawson>
2006-11-30 21:51:00 +0000

    fix syntax error.  add id3 line.

--- gengraphs.sh
+++ gengraphs.sh
@@ -10,7 +10,8 @@ if [ ! -f $RRDFILE ]; then
 fi
 
 
-DAYCOLOR="#aaaa00"
+#DAYCOLOR="#aaaa00"
+DAYCOLOR="#227722"
 # needs rrdtool-1.2.9 or higher.
 #export LANG=en_US.UTF-8
 #    --vertical-label="Temperature (°F)"
@@ -22,18 +23,23 @@ rrdtool graph $IMAGEFILE3 \
     --start="-3 day" \
     DEF:id1=$RRDFILE:id1:AVERAGE \
     DEF:id2=$RRDFILE:id2:AVERAGE \
+    DEF:id3=$RRDFILE:id3:AVERAGE \
     CDEF:id1f=id1,9,5,/,*,32,+ \
     CDEF:id2f=id2,9,5,/,*,32,+ \
+    CDEF:id3f=id3,9,5,/,*,32,+ \
     LINE1:id1f#ff0000:"attic" \
     LINE1:id2f#0000ff:"outside" \
+    LINE1:id3f#00ffff:"datacenter" \
+    HRULE:32$DAYCOLOR \
     VRULE:$(date -d 'today 0:00' +%s)$DAYCOLOR \
     VRULE:$(date -d '-1 day 0:00' +%s)$DAYCOLOR \
     VRULE:$(date -d '-2 day 0:00' +%s)$DAYCOLOR \
     VRULE:$(date -d '-3 day 0:00' +%s)$DAYCOLOR \
-    COMMENT:"\l" \
-    COMMENT:"currently: " \
+    COMMENT:"\\l" \
+    COMMENT:"currently " \
     GPRINT:id1f:LAST:"attic %.1lf" \
-    GPRINT:id2f:LAST:"outside %.1lf\r" >/dev/null
+    GPRINT:id2f:LAST:"outside %.1lf" \
+    GPRINT:id3f:LAST:"datacenter %.1lf\\r" >/dev/null
 
 
 
@@ -44,10 +50,14 @@ rrdtool graph $IMAGEFILE14 \
     --start="-14 day" \
     DEF:id1=$RRDFILE:id1:AVERAGE \
     DEF:id2=$RRDFILE:id2:AVERAGE \
+    DEF:id3=$RRDFILE:id3:AVERAGE \
     CDEF:id1f=id1,9,5,/,*,32,+ \
     CDEF:id2f=id2,9,5,/,*,32,+ \
+    CDEF:id3f=id3,9,5,/,*,32,+ \
     LINE1:id1f#ff0000:"attic" \
     LINE1:id2f#0000ff:"outside" \
+    LINE1:id3f#00ffff:"datacenter" \
+    HRULE:32$DAYCOLOR \
     VRULE:$(date -d 'today 0:00' +%s)$DAYCOLOR \
     VRULE:$(date -d '-1 day 0:00' +%s)$DAYCOLOR \
     VRULE:$(date -d '-2 day 0:00' +%s)$DAYCOLOR \
