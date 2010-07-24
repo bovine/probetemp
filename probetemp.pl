befa2036f3d8a59e0c0924bd21adec764b1e3ae1
Jeff Lawson <jeff@bovine.net>
2010-07-24 17:12:00 -0500

    warn if any of the port setup commands fail.

--- probetemp.pl
+++ probetemp.pl
@@ -4,15 +4,15 @@ use strict;
 use RRDs;
 use Device::SerialPort;
 
-my $PortName = "/dev/ttyS0";
+my $PortName = "/dev/ttyU0";
 
 my $PortObj = new Device::SerialPort ($PortName)
     || die "Can't open $PortName: $!\n";
 
-$PortObj->baudrate(2400);
-$PortObj->parity("none");
-$PortObj->databits(8);
-$PortObj->stopbits(1);
+$PortObj->baudrate(2400) || warn "baudrate";
+$PortObj->parity("none") || warn "parity";
+$PortObj->databits(8) || warn "databits";
+$PortObj->stopbits(1) || warn "stopbits";
 $PortObj->read_const_time(500);       # 500 milliseconds = 0.5 seconds
 $PortObj->read_char_time(0);          # avg time between read char
 
@@ -28,6 +28,7 @@ while (1) {
     #print "got $buffer\n";
     while ($buffer =~ s/^(.*?)[\n\r]+//s) {
 	my $line = $1;
+	#print "got \"$line\"\n";
 	my $timestamp = time();
 	$linesread++;
 
@@ -41,7 +42,6 @@ while (1) {
     }
     last if ($linesread >= 9 && scalar(keys(%lastread)));
 }
-
 
 # Write to a CSV file.
 open(FILE, ">>probetemp.txt") || die "failed to open log file";
